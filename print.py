0c22028dcd622de570a3eb3a56256e15ad13319b
sudheepcr <sudheep.cr@gmail.com>
2022-09-13 11:09:23 +0530

    Add files via upload
    Adding another file

---
+++ print.py
@@ -0,0 +1,8 @@
+# print statement
+name = "Sudheep"
+
+print("My name is: " + name)
+print("My name is:", name)
+print("My name is: %s" % name)
+print(f"My name is: {name}")
+print("My name is: {}".format(name))
